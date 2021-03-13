36c42631a9d26d0b13a6e53942b47609f641384c
Bird Egop <sampletext32@bk.ru>
2021-03-14 00:13:02 +0300

    Add project

---
+++ Sobolev_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sobolev_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = float.Parse(Console.ReadLine());
+            Console.Write("x=");
+            double x = float.Parse(Console.ReadLine());
+            Console.Write("B0=");
+            double B0 = float.Parse(Console.ReadLine());
+            Console.Write("Bk=");
+            double Bk = float.Parse(Console.ReadLine());
+            Console.Write("deltaB=");
+            double deltaB = float.Parse(Console.ReadLine());
+
+            for (double i = B0; i <= Bk; i += deltaB)
+            {
+                double R = (2.4 * Math.Cos(Math.Pow(a, 3) + Math.Sqrt(B0 * B0 - x))) / (Math.Pow(Math.E, a) + B0);
+
+                Console.WriteLine("При B" + i + " R=" + R);
+            }
+        }
+    }
+}
